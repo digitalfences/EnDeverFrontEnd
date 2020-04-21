6bd01cc1798caafefb6749f2c4c700e6933d6602
Daniel Edminster <Daniel.Edminster@outlook.com>
2020-04-21 08:59:26 -0400

    Sidebar styling

--- endever/src/App.js
+++ endever/src/App.js
@@ -5,15 +5,23 @@ import GitHubLogo from './img/GitHub_Logo_White.png';
 import './App.css';
 import './fonts/fonts.css';
 
+//For testing
+import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="landing">
+      <Navbar />
+      <Sidebar />
+      {/* <header className="App-header"> */}
+        
+        {/* <Sidebar /> */}
+        {/* <div className="landing">
           <img src={endevercircle} className="ProductLogo" /><div>EnDever App</div>
           </div>
           <div className="GitHubIntegration">Sign in with <img src={GitHubLogo} className="GitHubLogo" /></div>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ endever/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,32 @@
+import React, { Component  } from 'react';
+import '../../fonts/fonts.css';
+import './Sidebar.css';
+
+class Sidebar extends Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+    componentDidMount(){
+
+    }
+
+    render() {
+        return (
+            <div className="Sidebar">
+                <div className="Sidebar__Profile">
+                    
+                    <div>My Profile</div>
+
+                    </div>
+                
+            </div>
+        );
+    }
+}
+
+
+
+export default Sidebar;
