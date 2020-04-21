d6bc59a75064666bc5cdd52b63c7d9ed6d429837
Daniel Edminster <Daniel.Edminster@outlook.com>
2020-04-21 11:31:33 -0400

    Started work on DevCard component

---
+++ endever/src/components/DevCard/DevCard.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './DevCard.css';
+
+class DevCard extends Component {
+    constructor(){
+        super();
+        this.state = {
+
+        };
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+
+
+export default DevCard;
