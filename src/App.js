9e68fa609899604b93a6b27b1544aee6428ba4f7
Daniel Edminster <Daniel.Edminster@outlook.com>
2020-04-27 19:23:19 -0400

    hopefully deploying a fix

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ class App extends Component {
       let currentProfile = stateProfileArray.shift();
 
 
-      this.setState( { currentProfile: currentProfile } );
+      // this.setState( { currentProfile: currentProfile } );
 
 
 
@@ -90,16 +90,16 @@ class App extends Component {
 
   swipeRight = () => {
     // alert('yay');
-    // let stateProfileArray = this.state.profiles;
-    // let nextProfile = stateProfileArray.shift();
-    // this.setState({
-    //     currentProfile: nextProfile,
-    //     profiles: stateProfileArray
-    // })
+    let stateProfileArray = this.state.profiles;
+    let nextProfile = stateProfileArray.shift();
+    this.setState({
+        currentProfile: nextProfile,
+        profiles: stateProfileArray
+    })
 
-    // console.log("swiped right");
+    console.log("swiped right");
 
-    // let nextProfile = this.state.profiles[0];
+    let nextProfile = this.state.profiles[0];
 
 
   }
