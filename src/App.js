42b300f24a95bfa360ef483550bec7abe96f188d
Daniel Edminster <Daniel.Edminster@outlook.com>
2020-04-27 21:46:17 -0400

    removing async issue

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ class App extends Component {
     this.sessionCheck();
     this.getDevCardArray();
     this.setMainViewState(); 
-    this.getMyUserData();
+    // this.getMyUserData();
   }
 
   testStateChange = () => {
