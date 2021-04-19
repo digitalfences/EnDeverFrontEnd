f6d02cd2dcb5c77252a0a267a70365e49d0180d9
galen.emanuel.fencing@gmail.com <galen.emanuel.fencing@gmail.com>
2021-04-19 02:27:11 -0400

    update headers for CORS

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ const App = () => {
          .get("https://tigerkingbackend.herokuapp.com/sessioncheck", {
         withCredentials: true,
         headers: {
+          'Access-Control-Allow-Origin': '*',
           "Content-Type": "application/json",
         },
       })
